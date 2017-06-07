1a8400dc3ecb1198f2b888c53642f585d26a6fdf
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-07 02:45:11 -0500

    Updated for Swift 4
    Need to manually set `LIBRARY_SEARCH_PATHS = /usr/local/lib` in Xcode

--- Package.swift
+++ Package.swift
@@ -1,17 +1,39 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Cacao",
-    targets: [
-                 Target(
-                    name: "Demo",
-                    dependencies: [.Target(name: "Cacao")]),
-                 Target(
-                    name: "Cacao")
-    ],
+    /*exclude: ["Resources"],*/
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Cacao",
+            targets: ["Cacao"]
+        ),
+        .executable(
+            name: "Demo",
+            targets: ["Demo"]
+        )
+        ],
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/Silica.git", majorVersion: 1),
-        .Package(url: "https://github.com/PureSwift/CSDL2.git", majorVersion: 1)
+        .package(url: "https://github.com/PureSwift/CSDL2.git", .branch("master")),
+        .package(url: "https://github.com/PureSwift/SDL.git", .branch("master")),
+        .package(url: "https://github.com/PureSwift/Silica.git", .branch("master")),
+        .package(url: "https://github.com/PureSwift/Cairo.git", .branch("master"))
     ],
-    exclude: ["Xcode"]
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Cacao",
+            dependencies: ["Cairo", "Silica", "SDL"]),
+        .target(
+            name: "Demo",
+            dependencies: ["Cacao"]),
+        .testTarget(
+            name: "CacaoTests",
+            dependencies: ["Cacao"])
+        ]
 )
