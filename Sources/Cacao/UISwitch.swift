8dcdf162de64cccf591c8a47f707046c795bd0ca
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-15 03:37:46 -0500

    Working on UISwitch

---
+++ Sources/Cacao/UISwitch.swift
@@ -0,0 +1,57 @@
+//
+//  UISwitch.swift
+//  Cacao
+//
+//  Created by Alsey Coleman Miller on 6/15/17.
+//
+
+/// A control that offers a binary choice, such as On/Off.
+///
+/// The `UISwitch` class declares a property and a method to control its on/off state.
+open class UISwitch: UIControl {
+    
+    // MARK: - Initializing the Switch Object
+    
+    open init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    // MARK: - Setting the Off/On State
+    
+    /// A Boolean value that determines the off/on state of the switch.
+    ///
+    /// This property allows you to retrieve and set (without animation) a value
+    /// determining whether the `UISwitch` object is on or off.
+    public var isOn: Bool {
+        get { return _on }
+        set { setOn(newValue, animated: false) }
+    }
+    private var _on: bool = false
+    
+    /// Set the state of the switch to On or Off, optionally animating the transition.
+    ///
+    /// - Note: Setting the switch to either position does not result in an action message being sent.
+    public func setOn(_ on: Bool, animated: Bool) {
+        
+        _on = on
+    }
+    
+    // MARK: - Customizing the Appearance of the Switch
+    
+    /// The color used to tint the appearance of the switch when it is turned on.
+    public var onTintColor: UIColor?
+    
+    /// The color used to tint the outline of the switch when it is turned off.
+    // var tintColor: UIColor!
+    
+    /// The color used to tint the appearance of the thumb.
+    public var thumbTintColor: UIColor?
+    
+    /// The image displayed when the switch is in the on position.
+    public var onImage: UIImage?
+    
+    /// The image displayed while the switch is in the off position.
+    public var offImage: UIImage?
+}
