9b8bb28d2947f49ce1b47fa6779c09c92ac0fa10
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-11-24 16:16:02 -0500

    Implemented IOHIDEvent

--- Sources/Cacao/IOHIDEvent.swift
+++ Sources/Cacao/IOHIDEvent.swift
@@ -9,12 +9,106 @@ import Foundation
 import CSDL2
 import SDL
 
-internal final class IOHIDEvent {
+internal struct IOHIDEvent {
     
-    let sdlEvent: SDL_Event
+    let timestamp: UInt
     
-    init(sdlEvent: SDL_Event) {
+    let data: Data
+    
+    init?(sdlEvent: inout SDL_Event) {
         
-        self.sdlEvent = sdlEvent
+        self.timestamp = UInt(sdlEvent.common.timestamp)
+        
+        let eventType = SDL_EventType(rawValue: sdlEvent.type)
+        
+        switch eventType {
+            
+        case SDL_QUIT, SDL_APP_TERMINATING:
+            
+            self.data = .quit
+            
+        case SDL_MOUSEBUTTONDOWN,
+             SDL_MOUSEBUTTONUP,
+             SDL_MOUSEMOTION:
+            
+            //guard sdlEvent.touch
+            
+            let mouseEvent: MouseEvent
+            
+            switch eventType {
+                
+            case SDL_MOUSEBUTTONDOWN:
+                
+                mouseEvent = .down
+                
+            case SDL_MOUSEBUTTONUP:
+                
+                mouseEvent = .up
+                
+            case SDL_MOUSEMOTION:
+                
+                mouseEvent = .motion
+                
+            default:
+                
+                return nil
+            }
+            
+            let screenLocation = CGPoint(x: CGFloat(sdlEvent.button.x),
+                                         y: CGFloat(sdlEvent.button.y))
+            
+            self.data = .mouse(mouseEvent, screenLocation)
+            
+        case SDL_WINDOWEVENT:
+            
+            let sdlWindowEvent = SDL_WindowEventID(rawValue: SDL_WindowEventID.RawValue(sdlEvent.window.event))
+            
+            let windowEvent: WindowEvent
+            
+            switch sdlWindowEvent {
+                
+            case SDL_WINDOWEVENT_SIZE_CHANGED:
+                
+                windowEvent = .sizeChange
+                
+            case SDL_WINDOWEVENT_FOCUS_GAINED,
+                 SDL_WINDOWEVENT_FOCUS_LOST:
+                
+                windowEvent = .focusChange
+                
+            default:
+                
+                return nil
+            }
+            
+            self.data = .window(windowEvent)
+            
+        default:
+            
+            return nil
+        }
+    }
+    
+}
+
+internal extension IOHIDEvent {
+    
+    enum Data {
+        
+        case quit
+        case mouse(MouseEvent, CGPoint)
+        case touch
+        case window(WindowEvent)
+    }
+    
+    enum MouseEvent {
+        
+        case down, up, motion
+    }
+    
+    enum WindowEvent {
+        
+        case sizeChange
+        case focusChange
     }
 }
