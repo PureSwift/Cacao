3209ee9dc338b203043e6fc3cd700403e427398c
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-11-26 16:37:12 -0500

    Added UIKeyCommand

---
+++ Sources/Cacao/UIKeyCommand.swift
@@ -0,0 +1,21 @@
+//
+//  UIKeyCommand.swift
+//  Cacao
+//
+//  Created by Alsey Coleman Miller on 11/26/17.
+//
+
+import Foundation
+
+public final class UIKeyCommand {
+    
+    
+}
+
+// These are pre-defined constants for use with the input property of UIKeyCommand objects.
+
+public let UIKeyInputUpArrow = "UIKeyInputUpArrow"
+public let UIKeyInputDownArrow = "UIKeyInputDownArrow"
+public let UIKeyInputLeftArrow = "UIKeyInputLeftArrow"
+public let UIKeyInputRightArrow = "UIKeyInputRightArrow"
+public let UIKeyInputEscape = "UIKeyInputEscape"
